7acf98dc344f3a95d3109185116cb267f54ec220
Max Yaremchuk <38838852+w9w@users.noreply.github.com>
2020-11-04 15:38:56 +0200

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,19 @@
+printf "Subjs installation.. (Thanks to Corben Leo :) )\n"
+GO111MODULE=on go get -u -v github.com/lc/subjs
+
+printf "Gau installation.. (Thanks to Corben Leo :) )\n"
+GO111MODULE=on go get -u -v github.com/lc/gau
+
+printf "Hackrawler installation.. (Thanks to Luke Stephens :) )\n"
+GO111MODULE=on go get -u -v github.com/hakluke/hakrawler
+
+printf "github-endpoints.py wgetting.. (Thanks to Gwendal Le Coguic :) )\n"
+wget https://github.com/gwen001/github-search/raw/master/github-endpoints.py
+
+printf "SecretFinder installation.. (Thanks to @m4ll0k :) )\n"
+git clone https://github.com/w9w/SecretFinder.git
+
+printf "Arjun installation.. (Thanks to Somdev Sangwan :) )\n"
+git clone https://github.com/w9w/Arjun.git
+
+chmod -x automation_multitool_for_js.sh

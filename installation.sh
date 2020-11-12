4b6398a678e7a5d4e3c366af149df5eb011506e9
Max Yaremchuk <38838852+w9w@users.noreply.github.com>
2020-11-12 21:26:45 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -10,10 +10,13 @@ GO111MODULE=on go get -u -v github.com/hakluke/hakrawler
 printf "github-endpoints.py wgetting.. (Thanks to Gwendal Le Coguic :) )\n"
 wget https://github.com/gwen001/github-search/raw/master/github-endpoints.py
 
-printf "SecretFinder installation.. (Thanks to @m4ll0k :) )\n"
-git clone https://github.com/w9w/SecretFinder.git
+printf "Nuclei & httpx installation.. (Thanks to @m4ll0k :) )\n"
+GO111MODULE=on go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+
+GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
 
 printf "Arjun installation.. (Thanks to Somdev Sangwan :) )\n"
 git clone https://github.com/w9w/Arjun.git
 
 chmod +x automation_multitool_for_js.sh
+chmod +x automation/404_js_wayback.sh

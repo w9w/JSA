f9ed1063b8263916ddfbd23b86d55a5472eb8ae5
Max Yaremchuk <38838852+w9w@users.noreply.github.com>
2020-11-14 19:23:26 +0200

    Update 404_js_wayback.sh

--- automation/404_js_wayback.sh
+++ automation/404_js_wayback.sh
@@ -6,5 +6,5 @@ status_code=$(echo $url | httpx -x HEAD -status-code -silent -no-color | grep -o
 
 if [[ $status_code != 200 ]]
 then
-printf "https://web.archive.org/web/20060102150405/$url"
+printf "https://web.archive.org/web/20060102150405if_/$url\n"
 fi
